43b32800404764191d017ecdde1172d2ea528636
murod <murad.djanikulov@gmail.com>
2024-02-29 10:01:12 +0500

    fixed

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,6 +1,6 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/MuradjanDjanikulov/module1-git";
+    public static String REPO_LINK = "https://github.com/MuradjanDjanikulov/Module1-Git";
     //public static String REPO_LINK = "https://github.com/sergeytsoy740/Module1-Task3-Git";
 }
